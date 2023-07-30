2a3165b5ac708f5c633db84dabe951482a08323f
Salah Eddine <79339578+SalahEddine-Ghannouch@users.noreply.github.com>
2023-07-30 15:45:26 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,21 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+const count = document.getElementById("count");
+
+incrementVisitsCount();
+
+function incrementVisitsCount() {
+    let visits;
+
+    if (!localStorage.getItem("visits")) localStorage.setItem("visits", 1);
+    else {
+        visits = +localStorage.getItem("visits");
+        const incrementedCount = visits + 1;
+
+        localStorage.setItem("visits", incrementedCount);
+    }
+
+    count.innerText = localStorage.getItem("visits");
+}
